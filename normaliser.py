b0bdc3a5b41e617f1fdfbe178a35019ff61682e5
matej-veselovsky <matej.veselovsky97@gmail.com>
2022-07-02 20:25:35 +0200

    Define dropDescription()
    "Difficult" is used as a marker that there are no more meaningful
    entries in the current row. Once this string is found as an entry
    anything following can be, and will be discarded.

--- normaliser.py
+++ normaliser.py
@@ -1,4 +1,5 @@
 import sys
+import csv
 
 def replaceDelimiters(csv_file):
     data = ""
@@ -10,8 +11,29 @@ def replaceDelimiters(csv_file):
         file.truncate()
         file.write(data)
 
+def dropDescription(csv_file):
+    data = ""
+
+    with open(csv_file, mode="r", encoding="utf-8-sig") as file:
+        reader = csv.reader(file)
+
+        for row in reader:
+            for element in row: #iterates through all elements of each row
+                
+                if element != "Difficult":  # Once we get to "Difficult" there are no more translations in that row, and we can discard the rest
+                    data += element + ","
+                else: 
+                    data = data.rstrip(",") # Deletes the trailing comma
+                    break # continue in next row
+            data += "\n"
+    
+    with open(csv_file, mode="w") as file:
+        file.truncate()
+        file.write(data)
+
 def main(file):
     replaceDelimiters(file)
+    dropDescription(file)
 
 if (__name__ == "__main__"):
     file = str(sys.argv[1])
