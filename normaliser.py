94e8774aa0a59fa14acb4e0ed2995217be777560
matej-veselovsky <matej.veselovsky97@gmail.com>
2022-07-03 20:45:28 +0200

    insert feminine forms into original CSV
    These inserted forms have "0" as a first character, this will help
    in the next step, where we can use this to separate masculine and
    feminine forms into different rows.

--- normaliser.py
+++ normaliser.py
@@ -35,41 +35,50 @@ def dropDescription(csv_file):
 
 
 def createFeminine(csv_file):
-    feminineList = ""
+    data = ""
 
     with open(csv_file, mode="r", encoding="utf-8-sig") as file:
         reader = csv.reader(file)
 
         for row in reader:
-            if row[1][0] == "-":
-                stem = ""
-                masculineForm = row[0].strip()
-                feminineForm = ""
-                suffix = row[1][1:].strip()
-                match = suffix[0]
+            addition = ""
 
-                if masculineForm[0] == "-":
-                    continue
-                
-                if suffix == "cekaa":
-                    screener = len(masculineForm) - len("sikayi") - 1
-                elif suffix == "wiā":
-                    screener = len(masculineForm) - len("veyi") - 1
-                elif suffix == "liā":
-                    screener = len(masculineForm) - len("neyi") - 1
+            for element in row:
+                if element == row[1] and element[0] == "-":
+                    stem = ""
+                    masculineForm = row[0].strip()
+                    feminineForm = ""
+                    suffix = row[1][1:].strip()
+                    match = suffix[0]
+
+                    if masculineForm[0] == "-" or masculineForm[0] == "—":
+                        continue
+                    
+                    if suffix == "cekaa":
+                        screener = len(masculineForm) - len("sikayi") - 1
+                    elif suffix == "wiā":
+                        screener = len(masculineForm) - len("veyi") - 1
+                    elif suffix == "liā":
+                        screener = len(masculineForm) - len("neyi") - 1
+                    elif suffix == "huā":
+                        screener = len(masculineForm) - len("gayi") - 1
+                    elif suffix == "tuneā":
+                        screener = len(masculineForm) - len("neyi") - 1
+                    else:
+                        screener = len(masculineForm) - 1
+                        while (masculineForm[screener] != match):
+                            screener -= 1
+                    
+                    stem = masculineForm[0:screener]
+                    feminineForm = stem + suffix
+                    addition += "0" + feminineForm + ","
                 else:
-                    screener = len(masculineForm) - 1
-                    while (masculineForm[screener] != match):
-                        screener -= 1
-                
-                stem = masculineForm[0:screener]
-                feminineForm = stem + suffix
+                    addition += element + ","
+            data += addition.rstrip(",") + "\n"
 
-                feminineList += feminineForm + "\n"
-        
-    with open("./testfiles/feminine.txt", mode="w", encoding="utf-8-sig") as file:
+    with open(csv_file, mode="w", encoding="utf-8-sig") as file:
         file.truncate()
-        file.write(feminineList)
+        file.write(data)
 
 
 def main(file):
